d27eeefac7fa3bb7ccb51780614d11ea397b2b7c
ishu kumar <ishukumar200706@gmail.com>
2026-03-15 14:51:43 -0700

    file saved

--- animated/script.js
+++ animated/script.js
@@ -10,6 +10,7 @@ const locoScroll = new LocomotiveScroll({
 // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
 locoScroll.on("scroll", ScrollTrigger.update);
 
+
 // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
 ScrollTrigger.scrollerProxy("#main", {
   scrollTop(value) {
